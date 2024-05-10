582b311c6c63893d1efdd27aad011020e67128fd
Francesco Dagostino <francescodagostino1889@gmail.com>
2024-05-09 21:42:40 -0300

    carga de componente Home

--- ecommerce-proyect/src/App.jsx
+++ ecommerce-proyect/src/App.jsx
@@ -2,11 +2,13 @@ import Alerta from "./components/alerta/Alerta";
 import Card from "./components/card/Card";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-monumento-bandera">
       <Alerta />
+      <Home/>
     </div>
   );
 }
