6c50ee5cb8154a14baa72a80462eff78984f65c8
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-05-26 04:05:08 -0300

    carga del footer

--- ecommerce-proyect/src/App.jsx
+++ ecommerce-proyect/src/App.jsx
@@ -3,13 +3,17 @@ import Card from "./components/card/Card";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-monumento-bandera">
-      <Alerta />
-      <Home/>
-    </div>
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-monumento-bandera">
+        <Alerta />
+        <Home />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ ecommerce-proyect/src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white text-white py-6">
+      <div className="container mx-auto text-center">
+        <div className="flex justify-center space-x-4 mb-4">
+          <a href="#" className="text-gray-400 hover:text-white">
+            Home
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            About
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            Services
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            Contact
+          </a>
+        </div>
+        <div className="text-gray-400">
+          &copy; 2024 Your Company. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
