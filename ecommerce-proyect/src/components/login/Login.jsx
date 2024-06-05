6a0d161e47a83521bca27eb88aa9b44695ebfcad
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-06-05 01:04:36 -0300

    Agregue logica basica para el login

--- ecommerce-proyect/src/components/login/Login.jsx
+++ ecommerce-proyect/src/components/login/Login.jsx
@@ -1,27 +1,88 @@
+import { useState, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [erros, setErrors] = useState({
+    email: false,
+    password: false,
+  });
+
+  const navigate = useNavigate();
+
+  const { handleLogin } = useContext(AuthenticationContext);
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const changeEmailHandler = (event) => {
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
+  };
+
+  const changePasswordHandler = (event) => {
+    const inputPassword = event.target.value;
+    setPassword(inputPassword);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (emailRef.current.value.length === 0) {
+      emailRef.current.focus();
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: true,
+        password: false,
+      }));
+      return;
+    }
+
+    if (password.length === 0) {
+      passwordRef.current.focus();
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: false,
+        password: true,
+      }));
+      return;
+    }
+    handleLogin(email);
+    navigate("/");
+  };
   return (
     <>
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={submitHandler}
+        >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Username
+              Email
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" //agregar el color rojo si falta o esta mal con tailwind
+              ref={emailRef}
+              id="email"
+              onChange={changeEmailHandler}
               type="text"
-              placeholder="Username"
+              placeholder="Ingresa Email"
             />
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Password
+              Constraseña
             </label>
             <input
-              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" //agregar el color rojo si falta o esta mal con tailwind
+              ref={passwordRef}
               id="password"
+              value={password}
               type="password"
+              onChange={changePasswordHandler}
               placeholder="************"
             />
             <p className="text-red-500 text-xs italic hidden">
