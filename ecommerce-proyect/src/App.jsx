c938f5757c8aedddd3ece30e720855fa5399549c
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-06-05 01:01:52 -0300

    Agregue lo basico del router

--- ecommerce-proyect/src/App.jsx
+++ ecommerce-proyect/src/App.jsx
@@ -1,18 +1,30 @@
-import Alerta from "./components/alerta/Alerta";
-import Card from "./components/card/Card";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// import Protected from "./routes/Protected";
+// import Alerta from "./components/alerta/Alerta";
+// import Card from "./components/card/Card";
 import Login from "./components/login/Login";
-import Register from "./components/register/Register";
-import Home from "./components/home/Home";
-import Footer from "./components/footer/Footer";
+// import Register from "./components/register/Register";
+// import Home from "./components/home/Home";
+// import Footer from "./components/footer/Footer";
+import NotFound from "./routes/NotFound";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
   return (
     <>
       <div className="flex justify-center items-center min-h-screen bg-monumento-bandera">
-        <Alerta />
-        <Home />
+        <RouterProvider router={router} />
       </div>
-      <Footer />
     </>
   );
 }
