1f44dea6a3caaee8878aac04ae12c2824df9aca4
NachoBastianelli <nachobastianelli2003@gmail.com>
2024-07-19 14:05:43 -0300

    Carga de estilos para la seccion de pagos y footer

--- ecommerce-proyect/src/components/footer/Footer.jsx
+++ ecommerce-proyect/src/components/footer/Footer.jsx
@@ -5,21 +5,22 @@ const Footer = () => {
     <footer className="bg-white text-white py-6">
       <div className="container mx-auto text-center">
         <div className="flex justify-center space-x-4 mb-4">
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="/home" className="text-gray-400 hover:text-gray-800 ">
             Home
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-400 hover:text-gray-800 ">
             About
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-400 hover:text-gray-800 ">
             Services
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-400 hover:text-gray-800">
             Contact
           </a>
         </div>
         <div className="text-gray-400">
-          &copy; 2024 Dani Corp. All rights reserved.
+          &copy; 2024 <span className="font-bold"> Dani ❤️ </span> Corp. All
+          rights reserved.
         </div>
       </div>
     </footer>
--- ecommerce-proyect/src/components/paymentsSeccion/PaymentsSeccion.jsx
+++ ecommerce-proyect/src/components/paymentsSeccion/PaymentsSeccion.jsx
@@ -1,60 +1,125 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CreditCardInfo from "../creditCardInfo/CreditCardInfo";
 import InfoPayments from "../infoPayments/InfoPayments";
-import {
-  CartContext,
-  CartProvider,
-} from "../../services/cartContext/CartContext";
+import { CartContext } from "../../services/cartContext/CartContext";
 import { useNavigate } from "react-router-dom";
 import useToast from "../../hooks/useToast";
+import Confetti from "react-confetti";
 
 const PaymentsSeccion = () => {
   const { cart, clearCart } = useContext(CartContext);
   const navigate = useNavigate();
   const { showToast } = useToast();
   const [isCreditCardValid, setIsCreditCardValid] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [confetti, setConfetti] = useState(false);
 
-  const clickHandler = () => {
+  useEffect(() => {
+    if (isModalOpen) {
+      setConfetti(true);
+    } else {
+      setConfetti(false);
+    }
+  }, [isModalOpen]);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    navigate("/home");
+  };
+
+  const handleBuyClick = () => {
     if (isCreditCardValid) {
       clearCart();
       localStorage.removeItem("cart");
-      showToast("Compra realizada con exito!", true);
-      navigate("/home");
+      showToast("Compra realizada con éxito!", true);
+      handleOpenModal();
     } else {
       showToast("Datos de tarjeta inválidos", false);
     }
   };
 
   return (
-    <CartProvider>
-      <div className="flex flex-row ">
-        <div className="w-full flex justify-end mx-15 bg-gray-100 border-e-2 border-e-gray-200 items-center ">
-          <CreditCardInfo setIsValid={setIsCreditCardValid} />
+    <div className="flex flex-row">
+      <div className="w-full flex justify-end mx-15 bg-gray-100 border-e-2 border-e-gray-200 items-center">
+        <CreditCardInfo setIsValid={setIsCreditCardValid} />
+      </div>
+      <hr />
+      <div className="w-full flex flex-col justify-start mx-15">
+        <div>
+          <InfoPayments />
         </div>
-        <hr />
-        <div className="w-full flex flex-col justify-start mx-15">
-          <div>
-            <InfoPayments />
+        <div>
+          <div className="flex gap-2 justify-end max-w-xl mr-20 my-5">
+            <button
+              onClick={() => navigate("/home")}
+              className="py-2 px-4 bg-blue-500 rounded-lg flex justify-center items-center text-xl text-white cursor-pointer my-2 hover:bg-blue-600"
+            >
+              Home
+            </button>
+            <button
+              onClick={handleBuyClick}
+              className="py-2 px-4 bg-indigo-500 rounded-lg flex justify-center items-center text-xl text-white cursor-pointer my-2 hover:bg-indigo-600"
+            >
+              Buy
+            </button>
           </div>
-          <div>
-            <div className="flex gap-2 justify-end max-w-xl mr-20 my-5">
-              <button
-                onClick={() => navigate("/home")}
-                className="py-2 px-4 bg-blue-500 rounded-lg flex justify-center items-center text-xl text-white cursor-pointer my-2"
-              >
-                Home
-              </button>
-              <button
-                onClick={clickHandler}
-                className="py-2 px-4 bg-indigo-500 rounded-lg flex justify-center items-center text-xl text-white cursor-pointer my-2"
-              >
-                Buy
-              </button>
-            </div>
-          </div>
+          {isModalOpen && (
+            <>
+              <div
+                className="fixed inset-0 bg-[#D1FAE5] opacity-100"
+                onClick={handleCloseModal}
+              />
+              <div className="fixed inset-0 flex justify-center items-center z-20 ">
+                <div className="sm:w-[385px] sm:min-w-[40vw] min-w-[100vw] min-h-[30vh] flex flex-col items-center gap-2 p-6 bg-[#FFFFEB] rounded-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="text-[#059669] mx-auto h-11 rounded-full bg-[#D1FAE5] w-11"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span className="text-2xl font-medium">
+                    Payment Successful
+                  </span>
+                  <p className="text-center mt-3">
+                    Thank you for your purchase! 🛒
+                    <br />
+                    We appreciate your trust in us and hope you enjoy your
+                    purchase. ❤️
+                  </p>
+                  <button
+                    className="p-3 mt-6 bg-[#4F46E5] rounded-lg w-full text-white"
+                    onClick={handleCloseModal}
+                  >
+                    Back to home
+                  </button>
+                </div>
+              </div>
+              {confetti && (
+                <Confetti
+                  width={window.innerWidth}
+                  height={window.innerHeight}
+                  numberOfPieces={500}
+                  recycle={true}
+                  gravity={0.1}
+                />
+              )}
+            </>
+          )}
         </div>
       </div>
-    </CartProvider>
+    </div>
   );
 };
 
